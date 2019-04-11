fc1b5998e4f7f27c7f285e0a211524b01a766f80
Degreat Yartey <yarteydegreat2@gmail.com>
2019-04-11 16:22:42 +0100

    Float button, scroll up

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,25 @@
 $(document).ready(() => {
     var header = $('#home-nav');
+    var goUpButton = $('.go-up');
+
+    goUpButton.hide();
   
     $(window).scroll(() => {
       if ($(this).scrollTop() > 250) {
         header.fadeOut();
+        goUpButton.fadeIn();
       } else {
         header.fadeIn();
+        goUpButton.fadeOut();
       }
-    })
+    });
+
+    goUpButton.on('click', function(e) {
+      console.log("scrolling up...");
+      $('html, body').animate({
+        scrollTop: $('#top').offset().top
+      }, 500, 'linear');
+    });
 })
 
 AOS.init();
